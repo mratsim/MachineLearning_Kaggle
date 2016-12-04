e6142a96f632999a68184c0515e068d5649e9268
Mamy Ratsimbazafy <mamy.ratsimbazafy_dev@gadz.org>
2016-12-05 00:21:30 +0100

    with GridSearch

--- Kaggle - 001 - Titanic Survivors/Kaggle-001-Python-MagicalForest.py
+++ Kaggle - 001 - Titanic Survivors/Kaggle-001-Python-MagicalForest.py
@@ -9,10 +9,12 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
-from sklearn.base import TransformerMixin
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelBinarizer, RobustScaler, Binarizer, StandardScaler
 from sklearn_pandas import DataFrameMapper, cross_val_score
+from sklearn.model_selection import GridSearchCV
+from time import time
 
 train = pd.read_csv('./train.csv', delimiter=',', header=0) #Can't use column 0 Passenger ID as index, it's disappearing in the transform proc'
 test = pd.read_csv('./test.csv', delimiter=',', header=0)
@@ -31,7 +33,7 @@ test = pd.read_csv('./test.csv', delimiter=',', header=0)
 re_title = re.compile('(?<=, ).*?\.') #Get the title, for example Mrs.
 re_name = re.compile('^.*?,') #Get the name, for example Futrelle,
 
-class PP_TitleTransformer(TransformerMixin):
+class PP_TitleTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
     
@@ -43,18 +45,19 @@ class PP_TitleTransformer(TransformerMixin):
         return df.assign(CptTitle = df['Name'].map(lambda s:
                 re_title.search(s).group()))
 
-class PP_TitleCatTransformer(TransformerMixin):
+class PP_TitleCatTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
+        # 0, young, 1 unmarried, 2 normal, 3 high status
         self.dicoRef = {
             "Mr.": 2,
             "Mrs.": 2,
-            "Miss.": 0,
+            "Miss.": 1,
             "Master.": 0,
             "Don.": 3,
             "Rev.": 3,
             "Dr.": 3,
             "Mme.": 2,
-            "Ms.": 2,
+            "Ms.": 1,
             "Major.": 3,
             "Lady.": 3,
             "Sir.": 3,
@@ -76,7 +79,7 @@ class PP_TitleCatTransformer(TransformerMixin):
                           .map(lambda s:
                                self.dicoRef[s]))
 
-class PP_FamNameTransformer(TransformerMixin):
+class PP_FamNameTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
@@ -88,7 +91,7 @@ class PP_FamNameTransformer(TransformerMixin):
         return df.assign(CptName = df['Name'].map(lambda s:
                 re_name.search(s).group()))
 
-class PP_FamNameFreqTransformer(TransformerMixin):
+class PP_FamNameFreqTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.df_FamName = pd.DataFrame()
 
@@ -107,7 +110,7 @@ class PP_FamNameFreqTransformer(TransformerMixin):
         ) #TODO confirm if indexing match between df and dfX
 
 ####### Cabin Transformers ##########
-class PP_DeckTransformer(TransformerMixin):
+class PP_DeckTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
     
@@ -126,7 +129,7 @@ class PP_DeckTransformer(TransformerMixin):
 
 
 ####### Family Transformers ##########
-class PP_FamSizeTransformer(TransformerMixin):
+class PP_FamSizeTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
     
@@ -140,7 +143,7 @@ class PP_FamSizeTransformer(TransformerMixin):
             )
 
 ####### Fare Transformers ############
-class PP_FareImputer(TransformerMixin):
+class PP_FareImputer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.df_Fare = pd.DataFrame()
     
@@ -161,7 +164,7 @@ class PP_FareImputer(TransformerMixin):
         df = pd.DataFrame(X) #if using X directly it thinks it's a list ... dynalic typing ...
         return PP_FareImputer.combineFare(df,self.df_Fare)
 
-class PP_FareGroupTransformer(TransformerMixin):
+class PP_FareGroupTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.fare_range = np.concatenate(
             ([0],[1],np.arange(5,100,5),[np.inf])
@@ -177,7 +180,7 @@ class PP_FareGroupTransformer(TransformerMixin):
                 df['Fare'], bins=self.fare_range,labels=False, include_lowest=True
         ))
 
-class PP_FarePersonTransformer(TransformerMixin):
+class PP_FarePersonTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
     
@@ -191,7 +194,7 @@ class PP_FarePersonTransformer(TransformerMixin):
         )
 
 ####### Ticket Transformers ###########
-class PP_TicketFreqTransformer(TransformerMixin):
+class PP_TicketFreqTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.df_Ticket = pd.DataFrame()
 
@@ -203,14 +206,14 @@ class PP_TicketFreqTransformer(TransformerMixin):
         dfX = pd.DataFrame(X)
         df = dfX
         if not df.equals(self.df_Ticket):
-            df = pd.concat([self.df_Ticket, df])
+            df = pd.concat([self.df_Ticket, df]).reset_index()
         return dfX.assign(
             CptTicketFreq = df.groupby('Ticket')['Ticket']
                             .transform('count')
         ) #TODO confirm if indexing match between df and dfX
 
 ####### Embarked Imputer ############
-class PP_EmbarkedImputer(TransformerMixin):
+class PP_EmbarkedImputer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.df_Embarked = pd.DataFrame()
     
@@ -232,7 +235,7 @@ class PP_EmbarkedImputer(TransformerMixin):
         return PP_EmbarkedImputer.combineEmbarked(df,self.df_Embarked)
 
 ####### Age Transformers ############
-class PP_AgeGroupTransformer(TransformerMixin):
+class PP_AgeGroupTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.age_range = np.concatenate(
             ([0],np.arange(4,64,6),[np.inf])
@@ -248,7 +251,7 @@ class PP_AgeGroupTransformer(TransformerMixin):
                 df['Age'], bins=self.age_range,labels=False, include_lowest=True
         ))
 
-class PP_AgeGroupImputer(TransformerMixin):
+class PP_AgeGroupImputer(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.df_AgeGroup = pd.DataFrame()
     
@@ -270,20 +273,20 @@ class PP_AgeGroupImputer(TransformerMixin):
         df = pd.DataFrame(X) #if using X directly it thinks it's a list ... dynamic typing ...
         return PP_AgeGroupImputer.combineAgeGroup(df,self.df_AgeGroup)
 
-class PP_AgeImputer(TransformerMixin):
+class PP_AgeImputer(BaseEstimator, TransformerMixin):
     #depends from AgeGroup and CptTitleCat, so might introduce bias
     def __init__(self):
         self.df_Age = pd.DataFrame()
     
     def fit(self, X, y=None, **fit_params):
-        self.df_Age = X.groupby(['Pclass','Sex','CptTitleCat','CptAgeGroup'])['Age'] \
+        self.df_Age = X.groupby(['Pclass','Sex','CptTitleCat'])['Age'] \
                         .median() \
                         .reset_index()
         return self
 
     def combineAge(DF1, DF2):
         df2 = pd.DataFrame(DF2)
-        df = pd.merge(DF1,df2,on=['Pclass','Sex','CptTitleCat','CptAgeGroup'],how='left') #might also create level_3 column for empty join on CptTitleCat
+        df = pd.merge(DF1,df2,on=['Pclass','Sex','CptTitleCat'],how='left') #might also create level_3 column for empty join on CptTitleCat
         df['Age'] = df["Age_x"].fillna(df["Age_y"])
         #df.drop(['Age_x','Age_y'], axis=1, inplace=True)
         return df
@@ -292,7 +295,7 @@ class PP_AgeImputer(TransformerMixin):
         df = pd.DataFrame(X) #if using X directly it thinks it's a list ... dynamic typing ...
         return PP_AgeImputer.combineAge(df,self.df_Age)
 
-class PP_AgeClassTransformer(TransformerMixin):
+class PP_AgeClassTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
     
@@ -306,7 +309,7 @@ class PP_AgeClassTransformer(TransformerMixin):
         )
 
 ####### Debug Transformer ###########
-class DebugTransformer(TransformerMixin):
+class DebugTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
     
@@ -349,7 +352,7 @@ mapper = DataFrameMapper([
    ('CptDeck', LabelBinarizer()),
     (['CptTitleCat'], None),
 #    ('CptName', LabelBinarizer()),
-   (['CptNameFreq'], None),
+#    (['CptNameFreq'], None),
     (['CptFamSize'], None),
     (['CptFareGroup'], None),
     (['CptAgeGroup'], None),
@@ -364,24 +367,29 @@ mapper = DataFrameMapper([
 ######################################
 
 pipe = Pipeline([
+    ("extract_familyName",PP_FamNameTransformer()),
+    # ("extract_namefreq",PP_FamNameFreqTransformer()), #Buggy reindex issue on test data (but not on CrossVal)
     ("extract_deck",PP_DeckTransformer()),
     ("extract_ticketfreq",PP_TicketFreqTransformer()), #Note: for some reason ther eis a reindex error if put far down the pipeline
     ("extract_title", PP_TitleTransformer()),
     ("extract_titlecat", PP_TitleCatTransformer()),
-    ("extract_familyName",PP_FamNameTransformer()),
-    ("extract_namefreq",PP_FamNameFreqTransformer()),
     ("extract_famsize", PP_FamSizeTransformer()),
     ("fillNA_Fare", PP_FareImputer()),
     ("extract_faregroup", PP_FareGroupTransformer()),
     ("extract_fareperson", PP_FarePersonTransformer()),
-    ("extract_AgeGroup",PP_AgeGroupTransformer()),
-    ("fillNA_AgeGroup",PP_AgeGroupImputer()),
     ("fillNA_Age",PP_AgeImputer()),
+    ("extract_AgeGroup",PP_AgeGroupTransformer()),
+    #("fillNA_AgeGroup",PP_AgeGroupImputer()), #Useless if we fill age before
     ("fillNA_Embarked",PP_EmbarkedImputer()),
     ("extract_ageclass",PP_AgeClassTransformer()),
     ("DEBUG",DebugTransformer()),
      ("featurize", mapper),
-    ("forest", RandomForestClassifier())
+    ("forest", RandomForestClassifier(
+        n_estimators=15, 
+        max_features="sqrt", 
+        min_samples_split=18,
+        criterion= 'gini',
+        n_jobs=-1))
     ])
 
 ################ Training ################################
@@ -389,10 +397,31 @@ pipe = Pipeline([
 X_train = train
 y_train = train['Survived']
 
-##### Cross Validation
+# ##### Cross Validation
 crossval = cross_val_score(pipe, X_train, y_train, cv=10)
 print("Cross Validation Scores are: ", crossval.round(3))
 print("Mean CrossVal score is: ", round(crossval.mean(),3))
+
+# ##### GridSearch Tune hyperparameters #######
+# param_grid = { "forest__n_estimators"      : [1000], #[10,30,100,200, 300,1000],
+#            "forest__criterion"         : ["gini", "entropy"],
+#            "forest__max_features"      : [3, 5,"auto","sqrt","log2"],
+#            "forest__max_depth"         : [None, 10, 20],
+#            "forest__min_samples_split" : [2, 4],
+#         #    "forest__min_samples_leaf": [1, 3, 10, 50],
+#         #    "forest__bootstrap": [True, False],
+#         #    "forest__oob_score": [True,False]
+#            }
+
+# # run grid search
+# grid_search = GridSearchCV(pipe, param_grid=param_grid)
+# start = time()
+# grid_search.fit(X_train, y_train)
+
+# print("GridSearchCV took %.2f seconds for %d candidate parameter settings."
+#       % (time() - start, len(grid_search.cv_results_['params'])))
+# report(grid_search.cv_results_)
+
 
 ##### Fit ######
 pipe.fit(X_train, y_train)
@@ -416,3 +445,11 @@ top_features = top_features.sort_values('importance',ascending=False).set_index(
 # print(top_features)
 top_features.to_csv('top_features.csv')
 top_features.plot.bar()
+
+predictions = pipe.predict(test)
+result = pd.DataFrame({
+    'PassengerId': test['PassengerId'],
+    'Survived': predictions
+    })
+
+result.to_csv('python-magicalforest.csv', index=False)
